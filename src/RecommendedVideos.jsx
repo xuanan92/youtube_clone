e2c0e19ed6019331c7ab4ff468c73789fb1a68ed
root <root@XuanAnPC>
2023-07-09 00:46:43 +0700

    update

--- src/RecommendedVideos.jsx
+++ src/RecommendedVideos.jsx
@@ -16,6 +16,294 @@ export default function RecommendedVideos() {
           image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
         />
         <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.googleusercontent.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s176-c-k-c0x00ffffff-no-rj"
+          channel="Sonny Sangha"
+          image="https://i.ytimg.com/vi/UaB_FtsdlYk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDRiQb1ZdB7Kq4iBcc-bzpwkaMHQ"
+        />
+        <VideoCard
           title="Super Intelligence: Focus Music, Binaural Beats Concentration Music for Studying, Study Music"
           views="3.7M Views"
           timestamp="5 year ago"
